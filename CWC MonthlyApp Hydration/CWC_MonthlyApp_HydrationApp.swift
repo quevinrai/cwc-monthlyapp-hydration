c15feec03d57910cb5fa7caa970fb018efb93e81
Quevin Bambasi <quevinbambasi@pm.me>
2023-05-24 21:55:38 -0400

    Initial Commit

---
+++ CWC MonthlyApp Hydration/CWC_MonthlyApp_HydrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CWC_MonthlyApp_HydrationApp.swift
+//  CWC MonthlyApp Hydration
+//
+//  Created by Quevin Bambasi on 5/24/23.
+//
+
+import SwiftUI
+
+@main
+struct CWC_MonthlyApp_HydrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CWC MonthlyApp Hydration/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CWC MonthlyApp Hydration
+//
+//  Created by Quevin Bambasi on 5/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
